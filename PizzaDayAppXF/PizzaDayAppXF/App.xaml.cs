684a7edbbcf4234fb06250cb15f454350d17782e
Nadine Moser <nadine.moser@noser.com>
2018-09-07 13:07:58 +0200

    initial commit

---
+++ PizzaDayAppXF/PizzaDayAppXF/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace PizzaDayAppXF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=8f4468c9-36e0-4904-962c-7b0f64dd741f;",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ PizzaDayAppXF/PizzaDayAppXF/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PizzaDayAppXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            webView.Source = "https://pizzaday.noser.com/";
+        }
+    }
+}
